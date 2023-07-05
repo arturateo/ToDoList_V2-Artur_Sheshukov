5da601d025c0fbcd7fc10f1b5ddf7d01046cdf87
Artur Sheshukov <arti.shishka11@gmail.com>
2023-07-05 21:35:38 +0600

    Создал модели (Стасутов, Типов задач, Списка задач. Создал миграции

--- to_do_list_v2/models.py
+++ to_do_list_v2/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+TASKS = [('Task', 'Задача'), ('Bug', 'Ошибка'), ('Enhancement', 'Улучшение')]
+STATUS = [('New', 'Новый'), ('In Progress', 'В процессе'), ('Done', 'Выполнено')]
+
+
+class TasksModel(models.Model):
+    title = models.CharField(max_length=30, null=False, blank=False, verbose_name='Наименование', choices=TASKS)
+
+
+class StatusModel(models.Model):
+    title = models.CharField(max_length=30, null=False, blank=False, verbose_name='Наименование', choices=STATUS)
+
+
+class ToDoListModels(models.Model):
+    summary = models.CharField(max_length=60, null=False, blank=False, verbose_name='Заголовок')
+    description = models.TextField (max_length=300, null=True, blank=True, verbose_name='Название')
+    status = models.ForeignKey('to_do_list_v2.StatusModel', related_name='statuses', verbose_name='Статусы',
+                               on_delete=models.CASCADE)
+    task = models.ForeignKey('to_do_list_v2.StatusModel', related_name='tasks', verbose_name='Типы задач',
+                             on_delete=models.CASCADE)
+    create_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    update_date = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
