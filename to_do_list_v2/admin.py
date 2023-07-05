26d03c3c70cad000a31947f2f796cd8795982783
Artur Sheshukov <arti.shishka11@gmail.com>
2023-07-05 21:56:42 +0600

    Добавил модели в админку

--- to_do_list_v2/admin.py
+++ to_do_list_v2/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
-# Register your models here.
+from to_do_list_v2.models import TasksModel, StatusModel, ToDoListModels
+
+
+class TasksAdminModels(admin.ModelAdmin):
+    list_display = ['id', 'title']
+    list_display_links = ['id', 'title']
+    search_fields = ['title']
+    fields = ['title']
+
+
+admin.site.register(TasksModel, TasksAdminModels)
+
+
+class StatusAdminModels(admin.ModelAdmin):
+    list_display = ['id', 'title']
+    list_display_links = ['id', 'title']
+    search_fields = ['title']
+    fields = ['title']
+
+
+admin.site.register(StatusModel, StatusAdminModels)
+
+
+class ToDoListAdminModels(admin.ModelAdmin):
+    list_display = ['id', 'summary', 'description', 'status', 'task', 'create_date', 'update_date']
+    list_display_links = ['id', 'summary', 'description', 'status', 'task']
+    list_filter = ['summary', 'description', 'status', 'task']
+    search_fields = ['summary', 'description', 'status', 'task']
+    readonly_fields = ['create_date', 'update_date']
+    fields = ['summary', 'description', 'status', 'task', 'create_date', 'update_date']
+
+
+admin.site.register(ToDoListModels, ToDoListAdminModels)
--- to_do_list_v2/models.py
+++ to_do_list_v2/models.py
@@ -12,6 +12,9 @@ class TasksModel(models.Model):
         verbose_name = 'Типы задач'
         verbose_name_plural = 'Типы задач'
 
+    def __str__(self):
+        return self.title
+
 
 class StatusModel(models.Model):
     title = models.CharField(max_length=30, null=False, blank=False, verbose_name='Наименование', choices=STATUS)
@@ -21,13 +24,16 @@ class StatusModel(models.Model):
         verbose_name = 'Статусы'
         verbose_name_plural = 'Статусы'
 
+    def __str__(self):
+        return self.title
+
 
 class ToDoListModels(models.Model):
     summary = models.CharField(max_length=60, null=False, blank=False, verbose_name='Заголовок')
     description = models.TextField(max_length=300, null=True, blank=True, verbose_name='Название')
     status = models.ForeignKey('to_do_list_v2.StatusModel', related_name='statuses', verbose_name='Статусы',
                                on_delete=models.CASCADE)
-    task = models.ForeignKey('to_do_list_v2.StatusModel', related_name='tasks', verbose_name='Типы задач',
+    task = models.ForeignKey('to_do_list_v2.TasksModel', related_name='tasks', verbose_name='Типы задач',
                              on_delete=models.CASCADE)
     create_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     update_date = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
