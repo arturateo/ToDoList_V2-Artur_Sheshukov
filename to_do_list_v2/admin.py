7003628141cf0ec4143997e64510c053096e81e5
Artur Sheshukov <arti.shishka11@gmail.com>
2023-07-06 01:21:37 +0600

    Востановил работу миграций после не удачной датамиграции

--- to_do_list_v2/admin.py
+++ to_do_list_v2/admin.py
@@ -23,13 +23,13 @@ class StatusAdminModels(admin.ModelAdmin):
 admin.site.register(StatusModel, StatusAdminModels)
 
 
-class ToDoListAdminModels(admin.ModelAdmin):
-    list_display = ['id', 'summary', 'description', 'status', 'task', 'create_date', 'update_date']
-    list_display_links = ['id', 'summary', 'description', 'status', 'task']
-    list_filter = ['summary', 'description', 'status', 'task']
-    search_fields = ['summary', 'description', 'status', 'task']
-    readonly_fields = ['create_date', 'update_date']
-    fields = ['summary', 'description', 'status', 'task', 'create_date', 'update_date']
+# class ToDoListAdminModels(admin.ModelAdmin):
+#     list_display = ['id', 'summary', 'description', 'status', 'task', 'create_date', 'update_date']
+#     list_display_links = ['id', 'summary', 'description', 'status', 'task']
+#     list_filter = ['summary', 'description', 'status', 'task']
+#     search_fields = ['summary', 'description', 'status', 'task']
+#     readonly_fields = ['create_date', 'update_date']
+#     fields = ['summary', 'description', 'status', 'task', 'create_date', 'update_date']
 
 
-admin.site.register(ToDoListModels, ToDoListAdminModels)
+# admin.site.register(ToDoListModels, ToDoListAdminModels)
--- to_do_list_v2/models.py
+++ to_do_list_v2/models.py
@@ -33,7 +33,7 @@ class ToDoListModels(models.Model):
     description = models.TextField(max_length=300, null=True, blank=True, verbose_name='Название')
     status = models.ForeignKey('to_do_list_v2.StatusModel', related_name='statuses', verbose_name='Статусы',
                                on_delete=models.CASCADE)
-    task = models.ForeignKey('to_do_list_v2.TasksModel', related_name='tasks', verbose_name='Типы задач',
+    tasks = models.ForeignKey('to_do_list_v2.TasksModel', related_name='tasks', verbose_name='Типы задач',
                              on_delete=models.CASCADE)
     create_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     update_date = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
