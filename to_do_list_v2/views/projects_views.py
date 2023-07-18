2a3af4d2ae580699e5f4a3ca88b4951774d62e7d
Artur Sheshukov <arti.shishka11@gmail.com>
2023-07-18 22:47:39 +0600

    Исправил поиск задач в проекте

--- to_do_list_v2/views/projects_views.py
+++ to_do_list_v2/views/projects_views.py
@@ -50,11 +50,31 @@ class ProjectDetailView(DetailView):
     queryset = ProjectModels.objects.all()
     context_object_name = 'data'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+    def dispatch(self, request, *args, **kwargs):
+        self.form = self.get_search_form()
+        self.search_value = self.get_search_value()
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=None, **kwargs)
         tasks = ToDoListModels.objects.all()
-        context['tasks'] = tasks.filter(project_id=self.object.pk).distinct()
+        context["form"] = self.form
+        if self.search_value:
+            context["query"] = urlencode({'search': self.search_value})
+            context["search_value"] = self.search_value
+            context['tasks'] = tasks.filter(project_id=self.object.pk).filter(Q(summary__icontains=self.search_value) |
+                                                                              Q(description__icontains=self.search_value)).distinct()
+        else:
+            context['tasks'] = tasks.filter(project_id=self.object.pk).filter().distinct()
         return context
+
+    def get_search_form(self):
+        return SearchForm(self.request.GET)
+
+    def get_search_value(self):
+        if self.form.is_valid():
+            return self.form.cleaned_data['search']
+        return None
 
 
 class ProjectAddView(CreateView):
